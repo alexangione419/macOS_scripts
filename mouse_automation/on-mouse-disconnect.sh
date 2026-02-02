f6ec8cde661279daa79a87166ddaba0049d3e3da
Alex <alexangione419@gmail.com>
2026-02-02 13:22:16 -0500

    fixed defaults commands

--- mouse_automation/on-mouse-disconnect.sh
+++ mouse_automation/on-mouse-disconnect.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-defaults write "Apple Global Domain" com.apple.swipescrolldirection 1
+defaults write "Apple Global Domain" com.apple.swipescrolldirection -bool YES 
 killall cfprefsd 2>/dev/null || true
-
+/System/Library/PrivateFrameworks/SystemAdministration.framework/Resources/activateSettings -u
